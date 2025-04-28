a81c229262123538069499913f007530a1666fc1
Dipak Khade <dipakhade214@gmail.com>
2025-04-28 23:10:51 +0530

    added basic tool

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from mcp-server!")
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Demo")
+
+@mcp.tool()
+def myname()->str:
+    return "My name is Dipak R Khade"
+
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        mcp.run()
+    except KeyboardInterrupt:
+        print("Exiting")
+    
