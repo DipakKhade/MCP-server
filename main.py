fb700f58ed2ab1f7dd33e86f05c037a285b03512
Dipak Khade <dipakhade214@gmail.com>
2025-12-05 15:47:13 +0530

    listing projects

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
+import os
+from typing import List
 
-mcp = FastMCP("Demo")
+mcp = FastMCP("assistance")
 
 @mcp.tool()
-def myname()->str:
-    return "My name is Dipak R Khade"
-
-
+def totalProjects()->List[str]:
+    return os.listdir('/Users/dipakkhade/projects') 
 
 if __name__ == "__main__":
     try:
+        print('starting MCP Server...')
         mcp.run()
     except KeyboardInterrupt:
         print("Exiting")
